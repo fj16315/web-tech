fff2ebe7ac842fad183748402423cfc22aedc15a
Joe <Joseph.Mills22@gmail.com>
2019-05-21 16:59:07 +0100

    Adding missing node files

---
+++ site/public/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('p').click(function() {
+    $(this).hide();
+  })
+});
+
+/**
+function start(){
+  $('p').click(hideElement);
+}
+
+function hideElement(){
+  $(this).hide()
+}
+**/
