6857e527da420024104cddd81867658080347804
Freddie Jones <fj16315@my.bristol.ac.uk>
2019-05-22 14:49:32 +0100

    Refactored site to use Express

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 // Server which delivers only static HTML pages (no content negotiation).
 // Response codes: see http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 // When the global data has been initialised, start the server.
-let HTTP = require('http');
+/*let HTTP = require('http');
 let FS = require('fs').promises;
-let OK = 200, NotFound = 404, BadType = 415;
+let OK = 200, NotFound = 404, BadType = 415;*/
+
+let express = require('express');
+let app = express();
+
+let server = app.listen(8080, start);
+
+function start() {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log("%s:%s", host, port);
+}
+
+console.log("Before use");
+app.use(express.static('site/public'));
+console.log("After use");
+
+/*app.get('/index.html', function(req, res) {
+  console.log("Went on home page");
+  res.send('Hello World');
+});*/
+
+app.get('/index.html', function(req, res, next) {
+  let options = {
+    root: __dirname + '/site'
+  };
+  res.sendFile('/index.html', options, function(err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent file");
+    }
+  });
+});
 /*
 let types = {
   html : 'application/xhtml+xml',
@@ -13,7 +46,7 @@ let types = {
   jpg  : 'image/jpg'
 };
 */
-start(8080);
+/*start(8080);
 
 // Provide a service to localhost only.
 function start(port) {
@@ -76,4 +109,4 @@ function fail(response, code, message) {
   response.writeHead(code, hdrs);
   response.write(message);
   response.end();
-}
+}*/
