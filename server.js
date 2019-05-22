9d715b53b3f9a6b785ef28682330d4c80ab268f4
Freddie Jones <fj16315@my.bristol.ac.uk>
2019-05-22 19:06:33 +0100

    Login WIP

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // Server which delivers only static HTML pages (no content negotiation).
 // Response codes: see http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 // When the global data has been initialised, start the server.
-/*let HTTP = require('http');
-let FS = require('fs').promises;
-let OK = 200, NotFound = 404, BadType = 415;*/
 
 let express = require('express');
 let app = express();
+let sql = require('sqlite3');
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+let crypto = require('crypto');
+//let session = require('express-session');
+//let sqlstore = require('connect-sqlite3')(session);
+let db = new sql.Database("data.db");
 
 let server = app.listen(8080, start);
 
@@ -16,19 +20,47 @@ function start() {
   console.log("%s:%s", host, port);
 }
 
-console.log("Before use");
 app.use(express.static('site/public'));
-console.log("After use");
+app.use(passport.initialize());
+app.use(passport.session());
 
-/*app.get('/index.html', function(req, res) {
-  console.log("Went on home page");
-  res.send('Hello World');
-});*/
+passport.use(new LocalStrategy(function(username, password, done) {
+  console.log("Used passport!");
+  db.get('select salt from users where username = ?', username, function(err, row) {
+    if (!row) return done(null, false);
+    console.log("Password: " + password);
+    console.log("Salt: " + row.salt);
+    var hash = hashPassword(password, row.salt);
+    db.get('select username, id from users where username = ? and password = ?', username, hash, function(err, row) {
+      if (!row) return done(null, false);
+      return done(null, row);
+    });
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  return done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  db.get('select id, username from users where id = ?', id, function(err, row) {
+    if (!row) return done(null, false);
+    return done(null, row);
+  });
+});
+
+function hashPassword(password, salt) {
+  let hash = crypto.createHash('sha256');
+  hash.update(password);
+  hash.update(salt);
+  return hash.digest('hex');
+}
 
 app.get('/index.html', function(req, res, next) {
   let options = {
     root: __dirname + '/site'
   };
+
   res.sendFile('/index.html', options, function(err) {
     if (err) {
       next(err);
@@ -37,76 +69,19 @@ app.get('/index.html', function(req, res, next) {
     }
   });
 });
-/*
-let types = {
-  html : 'application/xhtml+xml',
-  css  : 'text/css',
-  js   : 'application/javascript',
-  png  : 'image/png',
-  jpg  : 'image/jpg'
-};
-*/
-/*start(8080);
 
-// Provide a service to localhost only.
-function start(port) {
-  let service = HTTP.createServer(handle);
-  try { service.listen(port, 'localhost'); }
-  catch (err) { throw err; }
-  console.log("Visit localhost:" + port);
-}
+app.get('/login.html', function(req, res, next) {
+  let options = {
+    root: __dirname + '/site'
+  };
 
-// Deal with a request.
-async function handle(request, response) {
-  let url = request.url;
-  if (url.endsWith("/")) url = url + "index.html";
-  if (! url.endsWith(".html") && ! url.endsWith(".css") && ! url.endsWith(".jpg") && ! url.endsWith(".jpeg") && ! url.endsWith(".png") && ! url.endsWith(".js") && ! url.endsWith(".ico")){
-    return fail(response, BadType, "Not .html or .css or other things");
-  }
-  let file = "./site/public" + url;
+  res.sendFile('/login.html', options, function(err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent file");
+    }
+  });
+});
 
-  let type;
-  if(url.endsWith(".html")){
-    type = 'application/xhtml+xml';
-  }
-  if(url.endsWith(".css")){
-    type = 'text/css';
-  }
-  if(url.endsWith(".png")){
-    type = 'image/png';
-  }
-  if(url.endsWith(".jpg")){
-    type = 'image/jpg';
-  }
-  if(url.endsWith(".jpeg")){
-    type = 'image/jpeg';
-  }
-  if(url.endsWith(".js")){
-    type = 'text/js';
-  }
-  if(url.endsWith(".ico")){
-    type = 'image/ico';
-    file = "./site/public/imgs/pot.png";
-  }
-
-  let content;
-  try { content = await FS.readFile(file); }
-  catch (err) { return fail(response, NotFound, "Not found"); }
-  reply(response, content, type);
-}
-
-// Send a reply.
-function reply(response, content, type) {
-  let hdrs = { 'Content-Type': type };
-  response.writeHead(OK, hdrs);
-  response.write(content);
-  response.end();
-}
-
-// Send a failure message
-function fail(response, code, message) {
-  let hdrs = { 'Content-Type': 'text/plain' };
-  response.writeHead(code, hdrs);
-  response.write(message);
-  response.end();
-}*/
+app.post('/login.html', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/index.html' }));
