4baabc165f91fde6374ef7f5dd6bba078768297f
Freddie Jones <fj16315@my.bristol.ac.uk>
2019-05-23 18:07:04 +0100

    Small update to database structure

--- create.sql
+++ create.sql
@@ -1,8 +1,11 @@
-select Ingredient, Quantity from Recipe, Recipe_Ingredient, Ingredients
+select IdR, Ingredient, Quantity from Recipe, Recipe_Ingredient, Ingredients
 where Recipe.IdR = Recipe_Ingredient.IdR
 and Ingredients.IdI = Recipe_Ingredient.IdI
 and Recipe.IdU = "1";
 
-select OrderNo, Step from Recipe, Steps
+select IdR, OrderNo, Step from Recipe, Steps
 where Recipe.IdR = Steps.IdR
 and Recipe.IdU = "1";
+
+select IdR, Title, Serves, Rating from Recipe
+where Recipe.IdU = "1";
