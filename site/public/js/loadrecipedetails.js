04b36f2deda3d2a6abb68fcf8edba4fdda855161
Joe <Joseph.Mills22@gmail.com>
2019-05-24 19:19:49 +0100

    Fixed navbar to be consistent between pages

--- site/public/js/loadrecipedetails.js
+++ site/public/js/loadrecipedetails.js
@@ -17,27 +17,25 @@ function loadRecipeDetails(str) {
       if (this.readyState == 4 && this.status == 200) {
         //for length generate each article and put it in an element
 
+        //Get json object from this this.response.jsonobject
         let a = JSON.parse(this.responseText);
         console.log(a);
-        //Get json object from this this.response.jsonobject
-        //foreach(recipe in response );
-        //$('searchResultsTitle').replaceWith('');
+
         //Get all the elements and put them in a search result
-        $('#loading').replaceWith('<div id="notLoading" class="col-xs-12 col-sm-6 col-md-12"><h2>Result for ' + str + '</h2><div id="results"><ul id="results"></ul></div></div>');
-        //replace the loading bar with these results
+        $('#loading').replaceWith('<div id="notLoading" class="col-xs-12 col-sm-6 col-md-12"><h2>Result for ' + str + '</h2><div id="results"></div></div>');
+        //replace the results with no results!
         if(a.titles.length == 0){
-          console.log('Bad');
-          $('#results').append('<li><p>No results!</p</li>');
+          $('#results').append('<p>No results!</p>');
         }
         else{
-          $('#results').append('<li><article class="search-result row"><div class="col-xs-12 col-sm-12 col-md-4"><a href="#" title="lorem-Ipsum" class="thumbnail"><img src="imgs/logos/logo.png" alt="Placeholder" ></a></div><div class="col-xs-12 col-sm-12 col-md-8"><ul class="meta-search"><h3><a href="/recipe_template.html?rID=1">' + a.titles[0] + '</a></h3><li><i class="glyphicon glyphicon-time"></i><span>a time?</span></li></ul></div><span class="clearfix borda"></span></aritcle></li>');
+          //foreach(recipe in response );
+          for(let i = 0; i<a.titles.length; i++){
+          $('#results').append('<article class="search-result row"><div class="col-xs-12 col-sm-12 col-md-4"><a href="' + '/recipe_template.html?rID=1' + '" title="' + a.titles[i] + '" class="thumbnail"><img src="imgs/logos/logo.png" alt="' + a.titles[i] + '" ></a></div><div class="col-xs-12 col-sm-12 col-md-8"><ul class="meta-search"><h3><a href="/recipe_template.html?rID=1">' + a.titles[i] + '</a></h3><li><i class="glyphicon glyphicon-time"></i><span>a time?</span></li></ul></div><span class="clearfix borda"></span></aritcle>');
+          }
         }
-        //Ingredients?
-        //'<article class="search-result row"><div class="col-xs-12 col-sm-12 col-md-4"><a href="#" title="lorem-Ipsum" class="thumbnail"><img src="imgs/logos/logo.png" alt="Placeholder" ></a></div><div class="col-xs-12 col-sm-12 col-md-8"><ul class="meta-search"><h3><a>' + str + '</a></h3><li><i class="glyphicon glyphicon-time"></i><span>a time?</span></li></ul></div><span class="clearfix borda"></span></aritcle>'
-        //Method?
       }
     };
-    xmlhttp.open("POST","getSearchResults" + "?search=" + str + "&" + "vegan=true",true);
+    xmlhttp.open("POST","getSearchResults" + "?search=" + str, true);
     xmlhttp.send();
   }
 }
