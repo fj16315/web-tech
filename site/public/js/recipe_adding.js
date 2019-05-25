a523d917f54dc4097c02b344fa9036cade111c06
Joe <Joseph.Mills22@gmail.com>
2019-05-25 20:00:00 +0100

    fixed add_recipe to send correct data in JSON

--- site/public/js/recipe_adding.js
+++ site/public/js/recipe_adding.js
@@ -7,6 +7,7 @@ function submitData(){
   let difficulty = $('#difficulty').val();
   let cookTime = $('#cookTime').val();
   let prepTime = $('#prepTime').val();
+  let serves = $('#serves').val();
 
   let ingredients = [];
   for(let i = 0; i < document.getElementById("ingredients").getElementsByTagName("li").length; i++){
@@ -22,17 +23,23 @@ function submitData(){
   console.log(method);
 
   //Get data, put it in a JSON object
-  let recipe = JSON.stringify({Title:title,Rating:difficulty,cookTime:cookTime,prepTime:prepTime,Serves:1,Ingredients:ingredients,Steps:method});
+  let recipe = JSON.stringify({Title:title,Rating:difficulty,CookTime:cookTime,PrepTime:prepTime,Serves:serves,Ingredients:ingredients,Steps:method});
 
   addRecipe(recipe);
 }
 
 function addIngredient(){
   let length = document.getElementById("ingredients").getElementsByTagName("li").length;
-  let ingredName = "ingredient" + length;
-  let ingredQuan = "quantity" + length;
-  console.log(length + ", " + ingredName + ", " + ingredQuan);
-  $('#ingredients').append('<li><div class="row"><div class="col-xs-8"><input type="text" class="form-control" id="' + ingredName + '" name="' + ingredName + '"/></div><div class="col-xs-4"><input type="text"  class="form-control" id="' + ingredQuan + '" name="' + ingredQuan + '"/></div></div></li>');
+  if(length < 20){
+    let ingredName = "ingredient" + length;
+    let ingredQuan = "quantity" + length;
+    let ingredType = "type" + length;
+    console.log(length + ", " + ingredName + ", " + ingredQuan);
+    $('#ingredients').append('<li><div class="row"><div class="col-xs-6"><input type="text" class="form-control" id="' + ingredName + '" name="' + ingredName + '"/></div><div class="col-xs-3"><input type="text"  class="form-control" id="' + ingredQuan + '" name="' + ingredQuan + '"/></div><div class="col-xs-3"><select class="form-control" id="' + ingredType + '" name=" ' + ingredType + '"><option value="tsp">Teaspoon(s)</option><option value="tbsp">Tablespoon(s)</option><option value="ml">ml</option><option value="grams">grams</option><option value="items">items</option></select></div></div></li>');
+  }
+  else{
+    alert("Too many ingredients!");
+  }
 }
 
 function removeIngredient(){
@@ -43,9 +50,14 @@ function removeIngredient(){
 
 function addMethod(){
   let length = document.getElementById("methods").getElementsByTagName("li").length;
-  let methodName = "method" + length;
-  console.log(methodName);
-  $('#methods').append('<li><input type="text" class="form-control" name="' + methodName + '" id="' + methodName + '"/></li>');
+  if(length < 20){
+    let methodName = "method" + length;
+    console.log(methodName);
+    $('#methods').append('<li><input type="text" class="form-control" name="' + methodName + '" id="' + methodName + '"/></li>');
+  }
+  else{
+    alert("Too many method steps!");
+  }
 }
 
 function removeMethod(){
