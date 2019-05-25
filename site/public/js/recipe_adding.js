65b79defe30c36c0843d846e64d05c40a6a73bc5
Joe <Joseph.Mills22@gmail.com>
2019-05-25 10:27:12 +0100

    Recipe adding WIP

---
+++ site/public/js/recipe_adding.js
@@ -0,0 +1,14 @@
+function addIngredient(){
+  $("#ingredients").append('<input name="newField" type="text">');
+}
+
+function removeIngredient(){
+
+}
+
+function addMethod(){
+  $("#method").append('<input name="newField" type="text">');
+}
+
+function removeMethod(){
+}
